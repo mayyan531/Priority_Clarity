9ac5df0de3aea46b3217a38e98cb2ee636dc3859
christinazzhang <156395205+christinazzhang@users.noreply.github.com>
2024-01-14 03:38:40 -0500

    Add files via upload

--- adding.js
+++ adding.js
@@ -1,13 +1,19 @@
-let input = document.getElementById("new-todo");
-let dateInput = document.getElementById("deadline");
+let input = document.getElementById("taskName");
+let dateInput = document.getElementById("Deadline");
 const addButton = document.getElementById("add-button");
+
 let taskList = [];
 load();
 
 //events
 addButton.addEventListener("click", add);
 
-function add(event) {
+function add() {
+    if (input === "") {
+        alert("Please enter a valid task");
+    } else if (dateInput === "") {
+        alert("Please enter in a valid date");
+    }
     //define object with task and deadline attributes
     let task = {
         todo: input.value,
@@ -34,16 +40,24 @@ function addBox({todo: input, category: date}) {
     let boxContainer = document.getElementById("box-container");
     let box = document.createElement("div");
     boxContainer.appendChild(box);
+    box.setAttribute('class', 'box');
 
     //add task name
     let taskName = document.createElement("h2");
     taskName.innerText = input;
     box.appendChild(taskName);
+    taskName.setAttribute('class', 'title');
 
     //add deadline
     let deadline = document.createElement("h3");
     deadline.innerText = date; //change to remaining days
     box.appendChild(deadline);
+    taskName.setAttribute('class', 'date');
+}
+
+//color picker
+function selectColor(hex) {
+    document.getElementById().style.backgroundColor = hex;
 }
 
 function save() {
