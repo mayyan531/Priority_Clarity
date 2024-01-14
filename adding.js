687d46dedede305c91534f64358db246e23eda5d
christinazzhang <christina.916.zhang@gmail.com>
2024-01-14 07:52:53 -0500

    s;ldkjf

--- adding.js
+++ adding.js
@@ -2,7 +2,8 @@ let input = document.getElementById("taskName");
 let dateInput = document.getElementById("Deadline");
 const addButton = document.getElementById("add-button");
 let boxes = document.querySelectorAll("box");
-
+selectColor;
+let colorhex;
 let taskList = [];
 load();
 
@@ -10,11 +11,6 @@ load();
 addButton.addEventListener("click", add);
 
 function add() {
-    if (input === "") {
-        alert("Please enter a valid task");
-    } else if (dateInput === "") {
-        alert("Please enter in a valid date");
-    }
     //define object with task and deadline attributes
     let task = {
         todo: input.value,
@@ -42,7 +38,11 @@ function addBox({todo: input, category: date}) {
     let box = document.createElement("div");
     boxContainer.appendChild(box);
     box.setAttribute('class', 'box');
-    box.setAttribute('id', 'box-' + taskList.length);
+    let newBox = 'box' + taskList.length;
+    box.setAttribute('id', newBox);
+
+    //add css property everytime a new box is made
+    //document.getElementById(newBox). [either colour(colour taken from function) or fit-content(% from due date difference)]
 
     //add task name
     let taskName = document.createElement("h2");
@@ -52,14 +52,37 @@ function addBox({todo: input, category: date}) {
 
     //add deadline
     let deadline = document.createElement("h3");
-    deadline.innerText = date; //change to remaining days
+    deadline.innerText = calculateDaysDifference(dateInput.value); //change to remaining days
     box.appendChild(deadline);
-    taskName.setAttribute('class', 'date');
+    deadline.setAttribute('class', 'date');
+
+    //set color
+    console.log(colorhex);
+    box.style.backgroundColor = colorhex;
 }
 
 //color picker
-function selectColor(hex) {
-    document.getElementById().style.backgroundColor = hex;
+function selectColor(colour) {
+    console.log("this function has been processed");
+    colorhex=colour;
+}
+
+function calculateDaysDifference(dateString) {
+    function parseDate(dateString) {
+        const parts = dateString.split('-');
+        const month = parseInt(parts[0], 10);
+        const day = parseInt(parts[1], 10);
+        const today = new Date();
+        const year = today.getFullYear();
+        const dateObject = new Date(year, month - 1, day);
+        return dateObject;
+    }
+
+    const today = new Date();
+    const userDate = parseDate(dateString);
+    const timeDifference = userDate - today;
+    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+    return daysDifference;
 }
 
 function save() {
